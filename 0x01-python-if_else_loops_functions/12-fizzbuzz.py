34dff13fb1d5bca4403c6078d52f1d1507413e3f
chinomso okoye <chinomsokoye@yahoo.com>
2022-05-31 08:03:35 -0700

    Write a function to print numbers 0-100 separated by space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
-            print("Fizzbuzz", end="")
+            print("Fizzbuzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         else:
-            print(i, end="")
+            print(i, end=" ")
