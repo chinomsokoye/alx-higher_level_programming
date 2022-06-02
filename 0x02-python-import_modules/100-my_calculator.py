8c54d93c0a457aade88496b93b7fa542c5c1420d
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-02 14:19:57 -0700

    Import all functions from a calculator file, handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if (operator is "+"):
-        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-    elif (operator is "-"):
-        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif (operator is "*"):
-        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-    elif (operator is "/"):
-        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+    if operator in "+-*/":
+        if (operator is "+"):
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        elif (operator is "-"):
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif (operator is "*"):
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        elif (operator is "/"):
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, *, /")
         sys.exit(1)
