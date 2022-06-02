28b2fa460197604b3c04ba0b48999786beebd69d
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-02 13:48:39 -0700

    Perform magic calculation, same as python bytecode

---
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition
+
+    Args:
+           a: first integer
+           b: second integer
+
+    Returns:
+           The return value. a + b
+    """
+    return a + b
+
+
+def sub(a, b):
+    """My subtraction
+
+    Args:
+           a: first integer
+           b: second integer
+
+    Returns:
+           The return value. a - b
+    """
+    return a - b
