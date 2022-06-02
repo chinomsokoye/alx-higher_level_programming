8eda4ee097677287555d743b1f4f6fa90b605791
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-02 14:07:00 -0700

    Import all functions from calculator file, handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,21 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    args = len(sys.argv)
-    if (args != 4):
+    my_args = len(sys.argv)
+    if (my_args != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int(sys.argv[1])
     operator = sys.argv[2]
+    a = int(sys.argv[1])
     b = int(sys.argv[3])
     if (operator is "+"):
-        print("{} {} {} = {}".format(a, b, operator, add(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif (operator is "-"):
-        print("{} {} {} = {}".format(a, b, operator, sub(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
     elif (operator is "*"):
-        print("{} {} {} = {}".format(a, b, operator, mul(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
     elif (operator is "/"):
-        print("{} {} {} = {}".format(a, b, operator, div(a, b)))
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, *, /")
         sys.exit(1)
