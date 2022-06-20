37d0e6de166bb2a0690a0932bcccbfa787e78d1e
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-20 11:57:14 -0700

    Write magic function for python bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+def magic_calculation(a, b):
+    res = 0
+    for x in range(1, 3):
+        try:
+            if (x > a):
+                raise Exception("Too far")
+            else:
+                res += (a ** b) / x
+        except:
+            res = a + b
+            break
+    return res
