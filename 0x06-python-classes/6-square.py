606da0dfbbff6dbf5a1493df378d7a083cd42ceb
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-21 10:39:56 -0700

    Define private size, position and public area of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -62,9 +62,9 @@ class Square:
         Args:
         value: sets position to tuple for value of positive integers, 2
         """
-        if type(value) is not tuple or len(value) != 2 or type(value[0]) \
-           is not int or type(value[1]) is not int or value[0] < 0 or \
-           value[1] < 0:
+        if type(value) is not tuple or len(value) != 2 or type(value[0])
+        is not int or type(value[1]) is not int or value[0] < 0
+        or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.position = value
