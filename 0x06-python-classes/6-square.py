f8eea3c07a30017d8d132571dc6cad24ed35fc87
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-21 10:57:28 -0700

    Define private size, position and public area of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,7 +69,7 @@ class Square:
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.position = value
+            self.__position = value
 
     def area(self):
         """Computes area of square
