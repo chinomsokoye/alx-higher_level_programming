558aa0fbdae7992fbd6bf439dab6fd59eb0ed7c8
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 07:25:36 -0700

    Return object is exactly as instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 2-is_same_class
+Contains methods
+Returns True if object is an instance of class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Defines function for is same instance"""
+    return type(obj) is a class else False
