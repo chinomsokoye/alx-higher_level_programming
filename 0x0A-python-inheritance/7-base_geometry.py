d3fb822aa6d7de5a9cd3f9f549a30531aee35441
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 15:14:48 -0700

    Write a class BaseGeometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Module 7-base_geometry
+"""
+Module 7-base_geometry
 Contains BaseGeometry
-Public method area, integer_validation
+Public instance method area, integer_validation
 """
 
 
 class BaseGeometry:
-    """Defines BaseGeometry class"""
-    def __init__(self):
-        """Initializes class BaseGeometry"""
-        pass
-
+    """Defines BaseGeometry class
+    Methods:
+    area(self)
+    integer_validator(self, name, value)
+    """
     def area(self):
-        """Raises Exception below"""
+        """Raises non implemented Exception below"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Integer validation for values of positives"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        """Validation for integer values
+        Args:
+        name (str): name always a string
+        value (int): greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
