eccf4e028fb86a04901c0d30269cc21adb965e88
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 13:14:37 -0700

    Return a class inheriting from another class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module 8-rectangle
+Contains class Rectangle
+"""
+
+
+class BaseGeometry:
+    """Defines the BaseGeometry class"""
+
+    def area(self):
+        """Raises Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validator for integer values"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""Rectangle class"""
+
+
+class Rectangle(BaseGeometry):
+    """Defines the Rectangle class"""
+    def __init__(self, width, height):
+        """Initializes Rectangle"""
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height
