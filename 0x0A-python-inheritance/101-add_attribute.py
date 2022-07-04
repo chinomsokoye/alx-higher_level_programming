9313bb81ea20c6c5752ef8227b39dd51123dd395
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 16:44:18 -0700

    Add a new attribute to an object if possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,8 +7,7 @@ Check to add attribute to object
 
 def add_attribute(an_obj, an_attr, a_value):
     """Checks if attribute of a value can be added to object"""
-    if not hasattr(an_obj, '__slots__') and not hasattr(an_obj, '__dict__'):
+    if ('__dict__' in dir(an_obj)):
+        setattr(an_obj, an_attr, a_value)
+    else:
         raise TypeError("can't add new attribute")
-    if not hasattr(an_obj, '__slots__') and not hasattr(an_obj, an_attr):
-        raise TypeError("can't add new attribute")
-    setattr(an_obj, an_attr, a_value)
