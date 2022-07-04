938b07a41b9bef72902dc03d2910960ab173c785
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 15:48:05 -0700

    Write a class inheriting from a class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """Module 10-square
-Contains 9-rectangle
+Contains a Square class created
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a class Square"""
-
+    """Defines a class Square inherited from Rectangle"""
     def __init__(self, size):
-        """Initializes the function"""
+        """Initializes the function for square"""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return string representation"""
+        return super().__str__()
+
+    def area(self):
+        """Return computational value of area"""
+        return self.__size ** 2
