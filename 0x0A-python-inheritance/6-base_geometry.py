ed16009c30ce1782f07130fa3bed7fec5ac766ea
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 08:03:17 -0700

    Write a class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 6--base_geometry
+Contains empty class
+Method, public area
+"""
+
+
+class BaseGeometry:
+    """Defines the BaseGeometry class"""
+    def area(Self):
+        """Raises an exception as below"""
+        raise Exception("area() is not implemented")
