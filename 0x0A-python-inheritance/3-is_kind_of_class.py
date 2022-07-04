90d7c8ddabca7149e57d29f54b265ff6d0215d96
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-04 07:37:16 -0700

    Return instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 3-is_kind_of_class
+Contains method
+Returns True if isinstance of class inherited from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns method of function"""
+    return isinstance(obj, a_class)
