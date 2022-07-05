44271cfaa6b9915a3fdbbd4288432746aba222a1
chinomso okoye <chinomsokoye@yahoo.com>
2022-07-05 16:18:08 -0700

    Create Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,12 +7,12 @@ Returns a list of integers representing pascals' triangle
 
 def pascal_triangle(n):
     """Creates the pascals' triangle"""
-    if n <= 0:
-        return []
-    if n == 1:
-        return [[1]]
-
-    pt == [[1]]
-    for rows in range(n - 1):
-        pt.append([x+y for x, y in zip([0] + pt[-1], pt[-1] + [0])])
+    pt = []
+    fn = []
+    for i in range(n):
+        nt = [1] + [sum(fn[elem:elem + 2]) for elem in range(len(fn) - 1)]
+        if i > 0:
+            nt = nt + [1]
+        pt.append(nt)
+        fn = nt
     return pt
