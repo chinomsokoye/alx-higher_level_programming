bfcb38b07244b368ed263b6fb944883eb0f36892
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-09 07:49:00 -0700

    List number of records with same score in table in MySQL server

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,4 +2,4 @@
 -- lists number of records with same score in the table in MySQL server
 
 SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score DESC;
+GROUP BY score ORDER BY number DESC;
