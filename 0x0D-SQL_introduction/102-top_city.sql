1ef5c0e98c1719150a0a5fd5fdf5ec963b481d44
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-09 08:50:16 -0700

    Display top 3 of cities temp during Jul and Aug ordered by temp desc

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,6 +3,6 @@
 --- temperature in desc
 
 SELECT city, AVG(value) AS avg_temp FROM temperatures
-WHERE month = 7 OR month = 8
+WHERE month = 7 AND month = 8
 GROUP BY city ORDER BY avg_temp DESC
 LIMIT 3;
