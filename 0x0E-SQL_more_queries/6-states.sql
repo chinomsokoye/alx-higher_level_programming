4cac06ed7b71bea54501bfbc8dc330a14c8c034f
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-10 14:19:08 -0700

    Create database and table on MySQL server, with description

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- SQL script
+-- creates the database hbtn_0d_usa and the table states
+-- in the MySQL server, with name and id description
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+name VARCHAR(256) NOT NULL
+);
