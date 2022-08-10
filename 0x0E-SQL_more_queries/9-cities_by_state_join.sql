d37f4b2288835fa5559072d519feda2ec6997cc3
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-10 14:57:56 -0700

    List all cities contained in the database of hbtn_0d_usa

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- SQL script
 -- lists all cities contained in the hbtn_0d_usa database
 
-SELECT city.id, city.name, st.name FROM cities city, states st
-WHERE city.state_id = st.id
-GROUP BY city.id ASC;
+SELECT city.id, city.name, state.name FROM cities city
+JOIN states state
+WHERE city.state_id = state.id
+ORDER BY city.id ASC;
