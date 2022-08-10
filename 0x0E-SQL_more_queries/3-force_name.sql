c317f26ab6c845d7d71106795b24f0c447b9d916
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-10 06:33:38 -0700

    Create the table force_name on MySQL server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- SQL script
+-- creates the table force_name on MySQL server
+-- description id INT, name VARCHAR(256) can't be null
+
+CREATE TABLE IF NOT EXISTS force_name (
+id INT,
+name VARCHAR(256) NOT NULL
+);
