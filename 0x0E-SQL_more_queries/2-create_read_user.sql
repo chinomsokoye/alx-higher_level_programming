e8f908d271c3e3fa95e9a1ea2ad50ada3d1e1c44
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-10 06:25:22 -0700

    Creates the database and user with only SELECT privilege on the database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- SQL script
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privileges in database hbtn_0d_2
+-- user_0d_2 password set to user_0d_2_pwd
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+FLUSH PRIVILEGES;
