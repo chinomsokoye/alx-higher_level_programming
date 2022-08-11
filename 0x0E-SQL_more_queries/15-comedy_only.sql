db33e12466956003fe52399f1fe18b3d70e9c97e
chinomso okoye <chinomsokoye@yahoo.com>
2022-08-10 17:31:39 -0700

    List all Comedy shows in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- SQL script
+-- list all comedy shows in the hbtn_0d_tvshows database
+
+SELECT tv_shows.title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
