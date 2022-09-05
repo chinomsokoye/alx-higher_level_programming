494d42e7e2156f30d750cdb4b622ab5fcaee5a8f
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-05 09:15:56 -0700

    Print a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length <= 1) {
+if (process.argv.length <= 2) {
   console.log('No argument');
-} else if (process.argv.length >= 2) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
