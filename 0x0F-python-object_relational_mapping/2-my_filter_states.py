f5a969a3a1ba5043e8d3113f9a0313bd9a899d8e
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-09 15:23:02 -0700

    Take and argument and displays all values of the state table from database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3])
 
     # create cursor to execute queries using SQL
-    # match argument given
+    # match the argument given
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
 ORDER BY states.id ASC".format(sys.argv[4]))
