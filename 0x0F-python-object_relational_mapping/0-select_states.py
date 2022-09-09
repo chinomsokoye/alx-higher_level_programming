cee76115bd4e94e6cd08c64ea4f069cc41383a47
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-09 14:34:21 -0700

    List all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    # connect to the database
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    # create cursor to execute queries using SQL
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
