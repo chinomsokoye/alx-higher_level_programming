3a03df7f6e6a39237a33adbe3a0067a33b3a0965
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-09 15:08:11 -0700

    Take and argument and displays all valuees of the state table from database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Takes in an argument and displays all values in the states table
+from the database hbtn_0e_0_usa
+where name matches the argument
+"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    # connect to the database
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    # create cursor to execute queries using SQL
+    # match argument given
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
