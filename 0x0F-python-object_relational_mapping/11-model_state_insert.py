1607f9a23da0d68ec3f7806743d0c8c1beda5650
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-11 16:53:30 -0700

    Add State object 'Louisiana' to the database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Adds the state object 'Louisiana' to the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    # create database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # query python in database
+    session = Session(engine)
+    new = State(name='Louisiana')
+    session.add(new)
+    new_state = session.query(State).filter(State.name == 'Louisiana').first()
+    session.commit()
+    print("{}".format(new_state.id))
+    session.close()
