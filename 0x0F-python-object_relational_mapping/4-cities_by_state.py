af8e0377cdac0e6031d314f1c85abdd52cf3ab34
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-11 18:36:53 -0700

    List all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     # create cursor to execute queries using SQL
     # joins two tables to retrieve cities
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
-FROM cities LEFT JOIN states\
-ON states.id = cities.state_id\
-ORDER BY cities.id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name
+FROM cities LEFT JOIN states
+ON states.id=cities.state_id
+ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
