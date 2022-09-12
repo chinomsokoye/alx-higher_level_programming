d8cb351dcbdf5421481a3e4f529989df25ca54b9
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-11 19:10:38 -0700

    Create state with the city from the database hbtn_0e_100_usa

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Create state with the city from the database hbtn_0e_100_usa
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    # create database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # query python in database
+    session = Session(engine)
+    new_city = City(name='San Francisco')
+    new = State(name='California')
+    new.cities.append(new_city)
+    session.add_all([new, new_city])
+    session.commit()
+    session.close()
