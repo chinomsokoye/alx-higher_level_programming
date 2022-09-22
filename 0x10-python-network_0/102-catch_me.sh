be5bcbd33b50606545eb28b304e439c6ad97acc9
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-22 13:28:27 -0700

    Make a request that cause server to respond with a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request, cause the server to respond with a message, in response body
-curl -s 0.0.0.0:5000/catch_me -X PUT -L -d -H "Origin: You got me!"
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
