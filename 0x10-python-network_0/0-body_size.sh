db22ae99ff521002f09f69bc5bea815e9277cdf9
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-22 07:00:34 -0700

    Display size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in URL, sends a request and display size of the response body
-curl -sI "$1" | grep 'Content-Length' | awk '{print $1}'
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
