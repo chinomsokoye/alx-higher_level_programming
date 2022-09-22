5bc4fd1f8e9e54693ae77770f88b2cfbc1ffe436
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-22 11:54:34 -0700

    Find a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,22 +2,18 @@
 """ Find a peak from a list of unsorted integers """
 
 
+def search(low, high, ints):
+    """ Defines the search """
+    mid = (low + high) // 2
+    if low == high:
+        return ints[high]
+    if ints[mid] < ints[mid + 1]:
+        return(search(mid + 1, high, ints))
+    return(search(low, mid, ints))
+
+
 def find_peak(list_of_integers):
     """ Find a peak of list_of_integers """
-    if list_of_integers is None or list_of_integers == []:
-        return None
-    low = 0
-    high = len(list_of_integers)
-    med = ((high - low) // 2) + low
-    mid = int(mid)
-    if high == 1:
-        return list_of_integers[0]
-    if high == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and \
-       list_of_integers[mid] >= list_of_integers[mid - 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    if not list_of_integers:
+        return
+    return(search(0, len(list_of_integers) - 1, list_of_integers))
