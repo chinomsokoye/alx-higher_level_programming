3faabd99b757955c450c6f8d2f3a8529d33e6655
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-23 07:45:30 -0700

    Take URL and email, sends POST request, display body of response

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Takes URL and email, sends POST request, display body of response """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.post(sys.argv[1], param={'email': sys.argv[2]})
+    print(r.text)
