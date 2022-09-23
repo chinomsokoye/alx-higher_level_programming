36fced8472b0c614565c690b915ac492a739d5f8
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-23 10:11:51 -0700

    List ten commits of a repository using GitHub API

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,11 +6,11 @@ from most recent to oldest """
 if __name__ == "__main__":
     import requests
     import sys
-    r = requests.get('https://developer.github.com/v3/repos/{}/{}/commits'.
-                     format(sys.argv[2], sys.argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[2], sys.argv[1]))
     if r.status_code >= 400:
         print('None')
     else:
-        for comm in r.json()[:10]:
-            print("{}: {}".format(comm.get('sha'), comm.get('commit').
-                                  get('author').get('name')))
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'), com.get('commit').get(
+                'author').get('name')))
