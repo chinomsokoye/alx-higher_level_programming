bbc4411f37061ae72dbeea26e496c797e7ee633e
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-18 15:14:04 -0700

    Print the title of the star wars movies

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Prints title of star wars movie
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
