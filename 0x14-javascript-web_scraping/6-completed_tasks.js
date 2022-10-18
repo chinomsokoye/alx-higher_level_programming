3b0f85a6b35142e7bccccbb9c47f25428de120dd
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-18 16:27:13 -0700

    Compute the number of tasks completed by user Id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Computes the number of tasks completed by user id
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = {};
+    for (const todo of JSON.parse(body)) {
+      if (todo.completed) {
+	    if (results[todo.userId] === undefined) { results[todo.userId] = 0; }
+        results[todo.userId] += 1;
+      }
+    }
+    console.log(results);
+  }
+});
