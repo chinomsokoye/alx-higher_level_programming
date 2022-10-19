8fa13be90199c6fbf34e59019aa5fe98d72eaad9
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-18 17:01:20 -0700

    Print all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 // Print all characters of a star wars movie
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const results = JSON.parse(body).characters;
-    for (const i of results) {
-      request(i, (a, b, c) => console.log(JSON.parse(c).name));
-    }
+request(url, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
   }
 });
