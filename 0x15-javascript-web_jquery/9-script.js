bf598431ef195fd270c183554eac45c801840faf
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-26 13:16:32 -0700

    Fetch from https://fourtonfish.com/hellosalut/?lang=fr and display the value of hello in the HTML tag DIV#hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('#hello').text(data.hello);
+    }
+  });
+});
